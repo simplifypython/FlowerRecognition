e12d86e0241d48f95de077e9a709b88364c90b91
Atul Prakash Singh <singh.prakash.atul@hotmail.com>
2018-12-02 12:48:54 +0530

    Added Step Comments

--- supervised_svc_algo.py
+++ supervised_svc_algo.py
@@ -1,4 +1,4 @@
-# import the required packages and modules
+# Step-1: import the required packages and modules
 import os
 import glob
 import numpy as np
@@ -11,31 +11,29 @@ from keras.preprocessing import image
 from PIL import Image
 
 
-# path to training & testing dataset
+# Step-2: set the path to training dataset
 train_path = "dataset/training"
 train_labels = os.listdir(train_path)
 
 
-# image datasets variables
+# Step-3: set image datasets variables
 image_size = (28, 28)
 actualimg_h, newimg_h = 28, 28
 colorscale = 'rgb'  # grayscale
 colorval = 3
 
 
-# variables to hold features and labels
+# Step-4: create variables to hold features and labels
 X = []
 Y = []
 
 
-# convert images into numeric lists by
-# looping over all the labels in the folder
+# Ste-5: convert images into numeric lists by looping over all the labels in the folder
 count = 1
 for i, label in enumerate(train_labels):
     cur_path = train_path + "/" + label
     count = 1
     for image_path in glob.glob(cur_path + "/*.jpg"):
-        # raise ValueError('color_mode must be "grayscale", "rbg", or "rgba"')
         img = image.load_img(
             image_path, target_size=image_size, color_mode=colorscale)
         img = image.img_to_array(img)
@@ -45,7 +43,7 @@ for i, label in enumerate(train_labels):
         count += 1
 
 
-# convert to np arrays
+# Step-6: convert features and labels to np arrays
 X = np.asarray(X)
 Y = np.asarray(Y)
 
@@ -53,35 +51,35 @@ print('Data shape: ', X.shape)
 print('Data label shape: ', Y.shape)
 
 
-# split the data into a training and testing set,
+# Step-7: split the data into a training and testing set,
 X_Train, X_Test, Y_Train, Y_Test = train_test_split(X, Y, random_state=0)
 
 print('Data shape: ', X_Train.shape)
 print('Data label shape: ', Y_Train.shape)
 
 
-# create the SVC model
+# Step-8: create the SVC model
 model = SVC(kernel='poly', C=0.1, max_iter=500000, gamma='auto')
 model.fit(X_Train, Y_Train)
 
 
-# predict the results from testing dataset
+# Step-9: predict the results from testing dataset
 Y_Pred = model.predict(X_Test)
 
 
-# finally, we can use the accuracy_score utility to see the fraction of
-# predicted labels that match their true value:
+# Step-10:
+# compare predicted result with actual labels
+# determine accuracy score
+# create confusion matrix
+# print the scores
 acc_score = accuracy_score(Y_Test, Y_Pred)
 print('accuracy score: ', acc_score)
 confusion_matrix(Y_Test, Y_Pred)
-
-
-# print(model)
 print('Training Score: {0}'.format(model.score(X_Train, Y_Train)))
 print('Testing Score: {0}'.format(model.score(X_Test, Y_Test)))
 
 
-# let's plot the result
+# Step-11: let's plot the result
 test_images = X_Test.reshape(-1, newimg_h, newimg_h, colorval)
 
 fig, axes = plt.subplots(6, 6, figsize=(56, 56), subplot_kw={
